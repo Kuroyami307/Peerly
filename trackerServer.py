2e7c5afcfcb84d3276c64ebdf55eb7a4c79c0a34
Kuroyami307 <utsav.dhakal307@gmail.com>
2026-07-01 22:56:03 +0545

    Peer to Peer Transfer Added

--- trackerServer.py
+++ trackerServer.py
@@ -10,38 +10,41 @@ PEER_INQUIRY = b'\xff'
 
 peer_list = []
 
+def recieve_data(peer_conn, message_size):
+    message = b""
+    temp_msg_size = message_size
+    recv_size = BUFFER
+    while True:
+        if temp_msg_size <= min(temp_msg_size, BUFFER):
+            message_frag = peer_conn.recv(temp_msg_size)
+            recv_size = len(message_frag)
+            message += message_frag
+
+            if recv_size < temp_msg_size:
+                temp_msg_size -= recv_size
+                continue
+
+            break
+            
+        message_frag = peer_conn.recv(BUFFER)
+        recv_size = len(message_frag)
+        message += message_frag
+        temp_msg_size -= recv_size
+
+    return message.decode('utf-8')
+
+
 def peer_thread_function(peer_conn, peer_addr):
     with peer_conn:
         packet_status = peer_conn.recv(1)
         print(packet_status)
         message_length = peer_conn.recv(4)
-        message = b""
-
         message_size = int.from_bytes(message_length, byteorder='big')
 
-        temp_msg_size = message_size
-        recv_size = BUFFER
-        while True:
-            if temp_msg_size <= min(temp_msg_size, BUFFER):
-                message_frag = peer_conn.recv(temp_msg_size)
-                recv_size = len(message_frag)
-                message += message_frag
-
-                if recv_size < temp_msg_size:
-                    temp_msg_size -= recv_size
-                    continue
-
-                break
-                
-            message_frag = peer_conn.recv(BUFFER)
-            recv_size = len(message_frag)
-            message += message_frag
-            temp_msg_size -= recv_size
-
-        message_str = message.decode('utf-8')
+        message_str = recieve_data(peer_conn, message_size)
 
         if packet_status == PEER_INFO:
-            peer_info = f"{peer_addr[0]}|{str(peer_addr[1])}|{message_str}"
+            peer_info = f"{peer_addr[0]}|{message_str}"
 
             lock = threading.Lock()
             
@@ -61,7 +64,10 @@ def peer_thread_function(peer_conn, peer_addr):
                     dpeer_ip, dpeer_port = results[0]
             
             dpeer_details = f"{dpeer_ip}|{dpeer_port}"
-            peer_conn.sendall(dpeer_details.encode('utf-8'))
+            dpeer_details_b = dpeer_details.encode('utf-8')
+
+            header = PEER_INFO + len(dpeer_details_b).to_bytes(4, byteorder='big')
+            peer_conn.sendall(header + dpeer_details_b)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind((HOST, PORT))
